dc1403b4c0b8e0c0ae0517434dec05f7644e0ac9
tim.hennessey <Tim.Hennessey@deutschinc.com>
2019-08-27 14:12:39 -0700

    Creative Update

--- app/300x250/scripts/Animation.js
+++ app/300x250/scripts/Animation.js
@@ -40,17 +40,7 @@ app.Animation = (function () {
 
 		.to(curtain, .75, {opacity: 1, onComplete: function () {tl2.play();}}, "+=2")
 
-		.from(house, .5, {opacity: 0, ease: Sine.easeInOut})
-
-		.from(z, .5, {x: "-=10", opacity: 0, ease: Sine.easeInOut}, "-=.3")
-		.from(i, .5, {x: "-=10", opacity: 0, ease: Sine.easeInOut}, "-=.3")
-		.from(l1, .5, {x: "-=10", opacity: 0, ease: Sine.easeInOut}, "-=.3")
-		.from(l2, .5, {x: "-=10", opacity: 0, ease: Sine.easeInOut}, "-=.3")
-		.from(o, .5, {x: "-=10", opacity: 0, ease: Sine.easeInOut}, "-=.3")
-		.from(w, .5, {x: "-=10", opacity: 0, ease: Sine.easeInOut}, "-=.3")
-
-		.from(tagline, 1, {y: "-=10", ease: Sine.easeInOut}, "-=1")
-		.from(tagline, .8, {opacity: 0}, "-=.8")
+		.to(txt3, .5, {y:"+=10", opacity: 1, ease: Sine.easeInOut})
 
 		.from(cta, 1, {scale: 0, opacity: 0, ease: Elastic.easeInOut}, "-=.25");
 		
