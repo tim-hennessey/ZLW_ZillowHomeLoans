d06bf49231105126bd22632e8c4f2914d50414fd
tim.hennessey <Tim.Hennessey@deutschinc.com>
2019-09-16 11:15:20 -0700

    Creative Update

---
+++ app/728x90/scripts/Animation.js
@@ -0,0 +1,79 @@
+var app = app || {};
+
+
+app.Animation = (function () {
+
+	
+	var t = TweenMax;
+	var tl1 = new TimelineMax();
+	var tl2 = new TimelineMax({paused:true});
+	var banner = document.getElementById('banner');
+	var txt1a = document.getElementById('txt1a');
+	var txt1b = document.getElementById('txt1b');
+	// var txt1c = document.getElementById('txt1c');
+
+	var txt2a = document.getElementById('txt2a');
+	var txt2b = document.getElementById('txt2b');
+	// var txt2c = document.getElementById('txt2c');
+	
+	var cta = document.getElementById('cta');
+	var curtain = document.getElementById('curtain');
+	var resolve = document.getElementsByClassName("resolve");
+	var container = document.getElementsByClassName("container");
+	
+
+	// --------------------------------------------------------------------------------------
+	// set default properties
+	function initialize() {
+		// DO NOT EDIT: reveals banner once loaded
+		t.set(banner, {opacity:1});
+		t.set(cta, {transformOrigin: "75% 50%"});
+		t.set(container, {y:"-=10"});
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Starts the animation
+	function start() {
+
+		tl1.to(txt1a, .5, {y:"+=10", opacity: 1, ease: Sine.easeInOut})
+		.to(txt1b, .5, {y:"+=10", opacity: 1, ease: Sine.easeInOut})
+		// .to(txt1c, .5, {y:"+=10", opacity: 1, ease: Sine.easeInOut})
+
+		.to(txt1a, .5, {opacity: 0}, "+=1.5")
+		.to(txt1b, .5, {opacity: 0}, "-=.5")
+		// .to(txt1c, .5, {opacity: 0}, "-=.5")
+
+		.to(txt2a, .5, {y:"+=10", opacity: 1, ease: Sine.easeInOut})
+		.to(txt2b, .5, {y:"+=10", opacity: 1, ease: Sine.easeInOut})
+		// .to(txt2c, .5, {y:"+=10", opacity: 1, ease: Sine.easeInOut})
+
+		.to(curtain, .75, {opacity: 1, onComplete: function () {tl2.play();}}, "+=1.5")
+
+		.set(resolve, {display: "block"})
+
+		.to(txt3, .5, {y:"+=10", opacity: 1, ease: Sine.easeInOut})
+		.to(container, .5, {y:"+=10", opacity: 1, ease: Sine.easeInOut}, "-=.5")
+
+		.to(cta, 0.1, {display: "block"})
+		.from(cta, .5, {scale: 0, opacity: 0, ease: Sine.easeInOut}, "-=.25");
+
+		tl2.to(flag, 0.1, {display: "block"})
+		.to(sprite, 1, {x: -468, ease: SteppedEase.config(18)});
+		
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Stops the animation
+	function stop() {
+		console.log("stopping animation");
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Publicly accessible methods and properties
+	return {
+		initialize:initialize,
+		start:start,
+		stop:stop
+	}
+
+})();
